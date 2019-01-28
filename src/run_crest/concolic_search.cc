sosy-lab/crest

commit f2406bd8035d546a8e33ed7b2b9ce79f7f51a627

Let crest write each created test into a separate file
The files are called input${RUN_COUNT}

src/run_crest/concolic_search.cc
@@ -22,6 +22,20 @@
 #include "base/yices_solver.h"
 #include "run_crest/concolic_search.h"
 
+#include <string>
+#include <sstream>
+
+namespace patch
+{
+    template < typename T > std::string to_string( const T& n )
+    {
+        std::ostringstream stm ;
+        stm << n ;
+        return stm.str() ;
+    }
+}
+
+
 using std::binary_function;
 using std::ifstream;
 using std::ios;
@@ -175,8 +189,9 @@ void Search::WriteCoverageToFileOrDie(const string& file) {
 }
 
 
+int runCount = 0;
 void Search::LaunchProgram(const vector<value_t>& inputs) {
-  WriteInputToFileOrDie("input", inputs);
+  WriteInputToFileOrDie("input" + patch::to_string(runCount++), inputs);
 
   /*
   pid_t pid = fork();

commit 4e3f44f7c56ff745cce00923b487cbabb23edb61

Let crest still write to the original file from which it reads inputs
Fixes a bug introduced in f2406bd, through which Crest
was not able to properly run concolic execution, as it tried
to read from a file "input" that never exist.

src/run_crest/concolic_search.cc
@@ -191,6 +191,7 @@ void Search::WriteCoverageToFileOrDie(const string& file) {
 
 int runCount = 0;
 void Search::LaunchProgram(const vector<value_t>& inputs) {
+  WriteInputToFileOrDie("input", inputs);
   WriteInputToFileOrDie("input" + patch::to_string(runCount++), inputs);
 
   /*